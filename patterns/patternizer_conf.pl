9b7c2fcc71c63a3199cf542f21e959e8c55fa4c8
cmungall <cjm@berkeleybop.org>
2018-09-02 19:16:09 -0700

    regen

--- patterns/patternizer_conf.pl
+++ patterns/patternizer_conf.pl
@@ -2,6 +2,7 @@
 
 :- rdf_register_prefix('UBERON','http://purl.obolibrary.org/obo/UBERON_').
 :- rdf_register_prefix('RO','http://purl.obolibrary.org/obo/RO_').
+:- rdf_register_prefix('BFO','http://purl.obolibrary.org/obo/BFO_').
 :- rdf_register_prefix('CL','http://purl.obolibrary.org/obo/CL_').
 :- rdf_register_prefix('PATO','http://purl.obolibrary.org/obo/PATO_').
 :- rdf_register_prefix('GO','http://purl.obolibrary.org/obo/GO_').
